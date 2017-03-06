52e5ee731b7a316a060f07b13dd69a8f5719d12d
Sangyeon <arsene96@naver.com>
2017-03-06 17:28:00 +0900

    Update Chapter10_Challenge4

--- chapter10/Challenge4.c
+++ chapter10/Challenge4.c
@@ -9,7 +9,34 @@
 
 #include <stdio.h>
 
+void calculate_cases(int money, int creambread_price, int shrimpcracker_price, int coke_price) {
+	//새우깡을 최대로 사면 money/shrimp_price해서 5이다.
+	//하지만 그러면 나머지가 0이다.
+	//적어도 하나 이상 구매해야한다.
+	//그러면 새우깡을 최대로 사면 3이다.
+	//근데 이걸 컴퓨터가 어떻게 알 수 있을까..
+}
+
+void choice_react(int choice) {
+	switch(choice) {
+		case 1:
+		case 2:
+		case 3: //case개수를 알수없다.
+	}
+}
+
 int main(void) {
+	int money = 3500;
+	int creambread_price = 500;
+	int shrimpcracker_price = 700;
+	int coke_price = 400;
+	int choice;
+
+	printf("현재 당신이 소유하고 있는 금액: %d\n", money);
+	calculate_cases(money, creambread_price, shrimpcracker_price, coke_price);
+	printf("어떻게 구입하시겠습니까? ");
+	scanf("%d", &choice);
+	choice_react(choice);
 
 	return 0;
 }
