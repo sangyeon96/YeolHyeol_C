dec660ffc333475034c1a83968e1797b51a07448
Sangyeon <arsene96@naver.com>
2017-03-08 23:15:42 +0900

    Update Chapter10_Challenge7

--- chapter10/Challenge7.c
+++ chapter10/Challenge7.c
@@ -1,11 +1,21 @@
-/*
-프로그램 사용자로부터 숫자 n을 입력받는다. 그리고 나서 다음 공식이 성립하는 k의 최댓값을 계산해서 출력하는 프로그램을 작성해보자
-2^k <= n
+#include <stdio.h>
 
-실행의 예
-상수 n 입력: 256
-공식을 만족하는 k의 최댓값은 8
+int main(void) {
+	int n, i;
+	int k = 1;
 
-도전7의 해결을 위한 GUIDE
-수식이 등장했다고 해서 두려워하지 말자. 위의 식을 그대로 반복문으로 옮겨 놓으면 쉽게 해결되는 문제이다.
-*/
+	printf("Input constant n. I'll print k which satisfies 2^k <= n : ");
+	scanf("%d", &n);
+
+	for(i = 0; ; i++) {
+		if(k <= n) {
+			k *= 2;
+			continue;
+		}
+		else
+			break;
+	}
+
+	printf("k which satisfies 2^k <= n is %d\n", i-1);
+	return 0;
+}
